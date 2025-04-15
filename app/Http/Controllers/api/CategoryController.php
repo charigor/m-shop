b179098381a587c44cb47118b81e5de522c131b8
Igor Charushnikov <cherychara@gmail.com>
2025-04-15 15:23:42 +0300

    added categories endpoint

--- app/Http/Controllers/api/CategoryController.php
+++ app/Http/Controllers/api/CategoryController.php
@@ -8,47 +8,8 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index($slug=null)
+    public function index()
     {
-
-        $category = Category::with(['media'])
-            ->selectRaw(
-                'categories.*,
-                                          category_lang.title,
-                                          category_lang.locale,
-                                          category_lang.description,
-                                          category_lang.meta_description,
-                                          category_lang.meta_keywords,
-                                          category_lang.meta_title,
-                                          category_lang.link_rewrite')
-            ->leftJoin('category_lang', 'category_lang.category_id', '=', 'categories.id')
-            ->where('locale', app()->getLocale())
-            ->when($slug, function ($query, $slug) {
-                return  $query->where('link_rewrite', $slug);
-            })
-            ->first();
-        if (! $category) {
-            return response()->json(['message' => 'Category not found'], 404);
-        }
-        if (! $category->children || $category->children->isEmpty()) {
-            $childCategories = Category::with(['media'])
-                ->selectRaw(
-                    'categories.*,
-                                          category_lang.title,
-                                          category_lang.locale,
-                                          category_lang.description,
-                                          category_lang.meta_description,
-                                          category_lang.meta_keywords,
-                                          category_lang.meta_title,
-                                          category_lang.link_rewrite')
-                ->leftJoin('category_lang', 'category_lang.category_id', '=', 'categories.id')
-                ->where('locale', app()->getLocale())
-                ->where('parent_id', $category->id)
-                ->get();
-
-            return response()->json(compact('category', 'childCategories'));
-        } else {
-            return response()->json( compact('category'));
-        }
+        return response()->json(Category::with(['children', 'translate'])->get()->toTree());
     }
 }
