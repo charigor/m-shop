ada8055b3e429944f2ab7f07d9bc986aaa4d1bde
Igor Charushnikov <cherychara@gmail.com>
2025-04-15 15:08:35 +0300

    added categories endpoint

--- app/Http/Controllers/api/CategoryController.php
+++ app/Http/Controllers/api/CategoryController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index($slug)
+    public function index($slug=null)
     {
 
         $category = Category::with(['media'])
