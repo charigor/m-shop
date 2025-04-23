493ba0a39f0166bab8bd177c9453e45b90c8d491
Igor Charushnikov <cherychara@gmail.com>
2025-04-23 14:38:55 +0300

    temp2

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Test\FactoryMethod\FactoryMethod;
+use App\Services\Patterns\FabricMethod\TelegramChannel;
 
 class TestController extends Controller
 {
     public function index(): string
     {
+
+        $channel = new TelegramChannel();
         return FactoryMethod::getConst();
     }
 }
